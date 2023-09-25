d5b1e470216a50e3b1dd99f8c9d69cbb3bbaae8a
bene <matheusbenediht@gmail.com>
2023-09-25 17:45:03 -0300

    Integrando modelo com API Flask

--- python-api/app.py
+++ python-api/app.py
@@ -1,10 +1,25 @@
 from flask import Flask, request, jsonify
+import joblib
+import numpy as np
 
+model = joblib.load('model/NaiveBayesAlzheimerPrediction.pkl')
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return 'Hello, world!'
+    return 'Model API!'
 
+@app.route('/predict', methods=['POST'])
+def predict_route():
+    data = request.get_json(force=True)
+
+    predict_data = [data['M/F'], data['Age'], data['EDUC'], data['SES'], data['MMSE'],
+                     data['CDR'], data['eTIV'], data['nWBV'], data['ASF']]
+
+    input = np.array(predict_data).reshape(1, -1)
+
+    predict = model.predict(input)
+
+    return jsonify({'result: ': predict.tolist()})
 if __name__ == '__main__':
     app.run()

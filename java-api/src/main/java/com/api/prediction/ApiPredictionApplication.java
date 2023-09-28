d4d90a23ed844ad0841f6288da7a62b48d9464d5
bene <matheusbenediht@gmail.com>
2023-09-28 20:25:47 -0300

    Update: Inicializando API Java

---
+++ java-api/src/main/java/com/api/prediction/ApiPredictionApplication.java
@@ -0,0 +1,13 @@
+package com.api.prediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPredictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPredictionApplication.class, args);
+	}
+
+}

a6e7beaa6a8f41cab23bcbe9c3e69a05be835f3d
bene <matheusbenediht@gmail.com>
2023-10-09 00:30:10 -0300

    Update: Tratativa de erro

--- java-api/src/main/java/com/api/prediction/services/user/DeleteUserService.java
+++ java-api/src/main/java/com/api/prediction/services/user/DeleteUserService.java
@@ -2,6 +2,7 @@ package com.api.prediction.services.user;
 
 import com.api.prediction.models.entities.UserEntity;
 import com.api.prediction.repositories.UserRepository;
+import com.api.prediction.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,11 @@ public class DeleteUserService {
     private UserRepository repository;
 
     public void delete(Long id){
-        repository.deleteById(id);
+        UserEntity user = findById(id);
+        repository.deleteById(user.getId());
+    }
+
+    public UserEntity findById(Long id){
+        return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("User not found!"));
     }
 }

e546e7f3b32ac47e51aa46cc725d24297c7028b3
bene <matheusbenediht@gmail.com>
2023-10-16 17:42:26 -0300

    Update: Refatorando código e realizando tratativa de exceção

--- java-api/src/main/java/com/api/prediction/services/pacient/GetDiagnosisService.java
+++ java-api/src/main/java/com/api/prediction/services/pacient/GetDiagnosisService.java
@@ -5,6 +5,7 @@ import com.api.prediction.models.dto.PredictionRequest;
 import com.api.prediction.models.dto.PredictionResponse;
 import com.api.prediction.models.entities.PacientEntity;
 import com.api.prediction.repositories.PacientRepository;
+import com.api.prediction.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -21,7 +22,7 @@ public class GetDiagnosisService {
     String url = "http://localhost:5000/predict";
 
     public PacientEntity addDiagnosis(Long id, PredictionRequest data){
-        PacientEntity pacient = repository.findById(id).get();
+        PacientEntity pacient = findById(id);
         PredictionResponse prediction = prediction(data);
         String diagnosisResult = prediction.result();
         pacient.setDiagnosis(String.valueOf(diagnosisResult ));
@@ -31,5 +32,9 @@ public class GetDiagnosisService {
     public PredictionResponse prediction(PredictionRequest data){
         ResponseEntity<PredictionResponse> response = restTemplate.restTemplate().postForEntity(url, data, PredictionResponse.class);
         return response.getBody();
+    }
+
+    public PacientEntity findById(Long id){
+        return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Pacient not found!"));
     }
 }

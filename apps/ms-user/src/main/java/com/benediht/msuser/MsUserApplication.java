29f0f7426b558794efb7fa960b6f0ad4db4a12ea
matheusBenedihtCaldeira <matheusbenediht@gmail.com>
2024-02-11 23:51:54 -0300

    Update: Adicionando Eureka server

--- apps/ms-user/src/main/java/com/benediht/msuser/MsUserApplication.java
+++ apps/ms-user/src/main/java/com/benediht/msuser/MsUserApplication.java
@@ -2,7 +2,9 @@ package com.benediht.msuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MsUserApplication {
 

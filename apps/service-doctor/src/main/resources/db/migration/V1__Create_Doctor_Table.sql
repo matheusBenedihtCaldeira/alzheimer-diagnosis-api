58770652aa9c24dc63b72637ea00d140ac14a77a
matheusBenedihtCaldeira <matheusbenediht@gmail.com>
2024-02-19 19:17:55 -0300

    Update: Migration criando tabela doctor

---
+++ apps/service-doctor/src/main/resources/db/migration/V1__Create_Doctor_Table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE tb_doctors(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    crm CHAR(13) NOT NULL UNIQUE,
+    cellphone CHAR(11) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+);

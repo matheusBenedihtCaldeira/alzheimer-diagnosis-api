8ac405749c586a0ff8f89328d0cbb3a7e6b2ee02
matheusBenedihtCaldeira <matheusbenediht@gmail.com>
2024-03-12 20:41:17 -0300

    Update: Iniciando serviço de paciente

---
+++ apps/service-patient/src/test/java/com/benediht/servicepatient/ServicePatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benediht.servicepatient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicePatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

e2726ba3f302b68b5ce4527f60b1d59e08c4ec15
matheusBenedihtCaldeira <matheusbenediht@gmail.com>
2024-03-25 19:40:36 -0300

    Update: Refazendo API principal do java

---
+++ alzheimer-diagnosis-api/src/main/java/com/alzheimer/diagnosis/api/JavaAlzheimerDiagnosisApiApplication.java
@@ -0,0 +1,13 @@
+package com.alzheimer.diagnosis.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAlzheimerDiagnosisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAlzheimerDiagnosisApiApplication.class, args);
+	}
+
+}
